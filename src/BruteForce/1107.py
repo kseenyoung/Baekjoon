4929eacba3cb2dc76832c6bffa21fef3e04749d3
kseenyoung <kseenyoung@gmail.com>
2023-03-20 22:24:32 +0900

    스스로 풀지 못 했던 문제 재도전

--- src/BruteForce/1107.py
+++ src/BruteForce/1107.py
@@ -12,7 +12,7 @@ for number in range(1000001):
     number = str(number)  # 포함 되는 숫자 중에 고장난 것이 없는지 확인 하기 위해
 
     for i, n in enumerate(number):
-        if int(n) in brokenButton:  # int 주의
+        if n in brokenButton:  # int 주의
             break
         if i == len(number) - 1:  # number 중 고장난 숫자 없음
             result = min(result, abs(int(number) - target) + len(number))
